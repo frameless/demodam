db8235708a13a417d025fbaf3a531a5f7f953733
Rids <74712581+Ridsko@users.noreply.github.com>
2023-02-06 14:59:43 +0100

    Gatsby install (#21)
    * Gatsby toegevoegd
    
    * Gatsby index veranderd naar Hello world

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `demodam2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
