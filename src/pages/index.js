08ea2d057445ddae8a0ccdc76ab9866ffdee87f1
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-09 10:44:48 +0100

    🔧 nl design system imports toegevoegd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
-import * as React from "react"
-import Header from "../components/header.js"
-
+import { Alert, Document, Heading1, Paragraph } from "@utrecht/component-library-react/dist/css-module";
+import * as React from "react";
+import Header from "../components/header.js";
+import "@utrecht/design-tokens/dist/index.css";
 
 const IndexPage = () => {
   return (
-    <Header>
-        
-    </Header>
-    // <main >
-    //   <h1>Hello world!</h1>
-      
-    // </main>
-  )
-}
+    <>
+      <Header></Header>
+      <main>
+        <h1>Hello world!</h1>
+      </main>
+      <Document></Document>
+      <Heading1></Heading1>
+      <Alert></Alert>
+      <Paragraph></Paragraph>
+    </>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home Page</title>;
