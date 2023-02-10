7c61cfd6387f6d088eef0276982fcceee8924c71
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-10 14:50:19 +0100

    ✨ added sections in contens.js

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,4 +1,41 @@
 import * as React from "react";
 
-const Content = () => <div></div>;
+const Content = () => (
+  <div>
+    <section>
+      <p>
+        Demodam is een fictieve gemeentewebsite die laat zien welke herbruikbare open source producten en diensten er
+        beschikbaar zijn voor gemeenten. Op Demodam.nl kun je straks zien welke Common Ground componenten (bouwstenen)
+        achter die diensten al klaar zijn en hoe ze met elkaar samenwerken. Gemeenten kunnen vervolgens besluiten om
+        zo`n dienst, bijvoorbeeld een applicatie voor online geboorteaangifte, ook in te zetten voor hun inwoners. Ook
+        is het mogelijk om verder te bouwen bovenop wat al beschikbaar is door zowel leveranciers als gemeenten.
+      </p>
+      <p>
+        Demodam wordt gemaakt en onderhouden door de Common Ground community, bestaande uit mensen en organisaties
+        vanuit zowel de overheid als het bedrijfsleven. De Foundation for Public Code ziet erop toe dat alle partijen
+        gelijkwaardig zijn. Iedereen is welkom om bij te dragen.
+      </p>
+      <h2>Demodam.nl is tijdelijk niet beschikbaar.</h2>
+      <p>
+        Wegens de log4j bug hebben we Demodam.nl tijdelijk offline gehaald. Gezien er ook een migratie op handen staat,
+        zullen we dit hierin meenemen. Vanaf eind januari 2022 komt Demodam.nl weer in de lucht. Onze excuses voor het
+        ongemak.
+      </p>
+    </section>
+    <section>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/jTK-sbee2qM"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
+    </section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+  </div>
+);
 export default Content;
