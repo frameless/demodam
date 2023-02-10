b57ab11dc0b9187c951af0a0cfe3441f7b98b6fa
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-10 11:51:55 +0100

    ♻️ changed log to icons

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const Logo = () => (
+const Icons = () => (
   <li>
     <a href="#">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
@@ -37,4 +37,4 @@ const Logo = () => (
     </a>
   </li>
 );
-export default Logo;
+export default Icons;
