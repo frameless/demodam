c55748e9b3357936bdb3581958bb77464e10f858
Rids <74712581+Ridsko@users.noreply.github.com>
2023-02-15 11:29:55 +0100

    fixup! 👷‍♂️ configure BASE_URL for GitHub Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `demodam2`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "demodam2",
+    siteUrl: "https://www.yourdomain.tld",
   },
   pathPrefix: process.env.BASE_URL ?? "/",
   plugins: [],
