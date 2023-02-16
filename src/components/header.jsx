c35846698b47dd43259d653a80a24a2db8914a77
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-16 15:23:42 +0100

    ♻️ icons naar github en slack veranderd en gesplitst

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import "/styles/header.style.css";
 import DemodamLogo from "./demodamlogo.jsx";
-import Icons from "./icons.jsx";
+import { Github, Slack } from "./icons.jsx";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -14,7 +14,6 @@ const Header = () => {
         <DemodamLogo role="img" aria-label="Logo van Demodam"></DemodamLogo>
       </a>
       <button
-        tabIndex={"tabIndex:0"}
         aria-expanded="true"
         className={`nav-burger ${open ? "active" : ""}`}
         onClick={() => {
@@ -62,20 +61,23 @@ const Header = () => {
               contact
             </a>
           </li>
-          <Icons></Icons>
           <li class="nav-item">
-            <ul>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  NL
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  EN
-                </a>
-              </li>
-            </ul>
+            <a href="#" class="nav-link" aria-label="github">
+              <Github></Github>
+            </a>
+            <a href="#" class="nav-link" aria-label="Slack">
+              <Slack></Slack>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              NL
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              EN
+            </a>
           </li>
         </ul>
       </nav>
