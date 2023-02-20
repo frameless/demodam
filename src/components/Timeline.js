16ed94edebfae2b2fc9d2dcec19f3075904ec9d1
Rids <74712581+Ridsko@users.noreply.github.com>
2023-02-20 13:33:55 +0100

    📱Timeline is reponsive gemaakt

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -6,11 +6,11 @@ const Timeline = () => {
     <>
       <h1 className="timelineheading">Tijdlijn</h1>
       <section className="timeline">
-        <div className="container right-container">
+        <div className="containert left">
           <section className="textbox">
             <hgroup>
-              <h3>Innovatiesubsidie toegekend</h3>
-              <p>Juli 2021</p>
+              <p className="datum">Juli 2021</p>
+              <h3 style={{ margin: "1rem 0px 0.75rem" }}>Innovatiesubsidie toegekend</h3>
             </hgroup>
             <p>
               Het ministerie van Binnenlandse Zaken en Koninkrijksrelaties heeft Demodam een subsidie van 150.000 euro
@@ -19,33 +19,37 @@ const Timeline = () => {
           </section>
         </div>
 
-        <div className="container left-container">
+        <div className="containert right">
           <section className="textbox">
-            <small>5 Juli tot 9 Juli 2021</small>
-            <h3>Eerste demodam hackathon</h3>
+            <hgroup>
+              <p className="datum">5 Juli tot 9 Juli 2021</p>
+              <h3 style={{ margin: "1rem 0px 0.75rem" }}>Eerste demodam hackathon</h3>
+            </hgroup>
             <p>Samen werken aan een commonground etalage</p>
+            <br />
+            <a href="#">Informatie en inschrijven</a>
           </section>
         </div>
 
-        <div className="container right-container">
-          <div className="textbox">
-            <hgroup>
-              <h3>Tweede demodam hackathon</h3>
-              <p>24 Juni 2021 13:00</p>
-            </hgroup>
-            <p>Onboarding en planning</p>
-            <p></p>
-          </div>
-        </div>
-
-        <div className="container left-container">
+        <div className="containert left">
           <section className="textbox">
             <hgroup>
-              <h2>Kick-off Demodam</h2>
-              <p>Juli 2021</p>
+              <p className="datum">24 Juni 2021 13:00</p>
+              <h3 style={{ margin: "1rem 0px 0.75rem" }}>Tweede demodam hackathon</h3>
+            </hgroup>
+            <p>Onboarding en planning</p>
+            <br />
+            <a href="#">Informatie en inschrijven</a>
+          </section>
+        </div>
+
+        <div className="containert right">
+          <section className="textbox">
+            <hgroup>
+              <p className="datum">Juli 2021</p>
+              <h3 style={{ margin: "1rem 0px 0.75rem" }}>Kick-off Demodam</h3>
             </hgroup>
             <p>Lancering van Demodam</p>
-            <p></p>
           </section>
         </div>
       </section>
