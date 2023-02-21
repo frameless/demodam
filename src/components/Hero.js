84a47f399c73056935403f308634aeb32c885a06
Rids <74712581+Ridsko@users.noreply.github.com>
2023-02-21 14:52:15 +0100

    💄 Hero headings vervangen met nlds componenten

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,29 @@
+import { Heading1, Heading3 } from "@utrecht/component-library-react";
 import * as React from "react";
 import "./hero.css";
 
 const Hero = () => {
   return (
-    <div className="herotext">
-      <hgroup>
-        <h1>Demodam</h1>
-        <p>Een digitale gemeente gebouwd op open source commonground componenten</p>
+    <div className="hero-container">
+      <hgroup className="container">
+        <Heading1 className="heading1">Demodam</Heading1>
+        <br />
+        <Heading3 className="heading3">Een digitale gemeente gebouwd op open source commonground componenten</Heading3>
+        <br />
+        <p className="content-hero">
+          Demodam is een fictieve gemeentewebsite die laat zien welke herbruikbare open source producten en diensten er
+          beschikbaar zijn voor gemeenten.
+        </p>
+        <br />
+        <p>
+          <a
+            href="https://commonground.nl/blog/view/6c422c0a-e541-41c8-8261-9c8bc90f4d87/doe-mee-met-demodam-de-common-ground-showcase-website"
+            className="lowerlink"
+          >
+            Lees hier meer over Demodam
+          </a>
+        </p>
       </hgroup>
-      <p>
-        Demodam is een fictieve gemeentewebsite die laat zien welke herbruikbare open source producten en diensten er
-        beschikbaar zijn voor gemeenten.
-      </p>
-      <p>
-        <a
-          href="https://commonground.nl/blog/view/6c422c0a-e541-41c8-8261-9c8bc90f4d87/doe-mee-met-demodam-de-common-ground-showcase-website"
-          style={{ color: "black" }}
-        >
-          Lees hier meer over Demodam
-        </a>
-      </p>
       <p>
         <img
           src={require("./hero.png").default}
