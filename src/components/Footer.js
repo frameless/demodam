94bf9b4fe98c378487281cd393c5073485242cef
Rids <74712581+Ridsko@users.noreply.github.com>
2023-02-21 13:55:31 +0100

    💄 Footer styling toegevoegd

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,15 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer className="main-footer">
-      <div className="container">
+      <div className="containerf">
         <div className="row">
           <ul className="list-unstyled">
-            <li>Footer info</li>
-            <li>Footer info</li>
-            <li>Footer info</li>
+            <li>Home</li>
+            <li>Sluit je aan</li>
+            <li>Hackathons</li>
+            <li>Organisaties</li>
+            <li>Governance</li>
+            <li>Contact</li>
           </ul>
         </div>
       </div>
