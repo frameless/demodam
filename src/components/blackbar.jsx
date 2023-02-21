eb80cdeee256d906f1ba03712d406832e024ccb9
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-21 14:27:10 +0100

    ✨ image and background added

--- src/components/blackbar.jsx
+++ src/components/blackbar.jsx
@@ -1,8 +1,30 @@
+import { Image } from "@utrecht/component-library-react";
 import * as React from "react";
+import conduction from "../images/conduction.png";
+import "/styles/blackbar.css";
 
 const Blackbar = () => (
-  <section>
-    <div></div>
+  <section className="blackbar-section">
+    <div className="blackbar-container">
+      <div className="blackbar-image-block">
+        <a>
+          <Image
+            className="utrecht-img--scale-down"
+            alt="illustratie van een persoon die een checklijst afvinkt"
+            src={conduction}
+          />
+        </a>
+        <a>
+          <Image />
+        </a>
+        <a>
+          <Image />
+        </a>
+        <a>
+          <Image />
+        </a>
+      </div>
+    </div>
   </section>
 );
 export default Blackbar;
