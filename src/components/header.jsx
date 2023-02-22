f8b341a707fd0381ead208bcd72b27eb00d89aa6
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-22 11:25:34 +0100

    🎨 header de contributing page toegevoegd

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
             </Link>
           </li>
           <li class="nav-item">
-            <Link href="#" class="nav-link">
+            <Link href="contributing" class="nav-link">
               Sluit je aan
             </Link>
           </li>
