4c93e612817ec1d3e548d93b80be7351d4779d43
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-22 14:13:50 +0100

    💄 classes met styling toegevoegd

--- src/components/contribution-content.jsx
+++ src/components/contribution-content.jsx
@@ -3,10 +3,11 @@ import { Heading2 } from "@utrecht/component-library-react";
 import { Link } from "gatsby";
 import * as React from "react";
 import "/styles/contribution.css";
+import "/styles/alignment.css";
 
 const ContributionContent = () => {
   return (
-    <div className="contribute-containers">
+    <div className="contribute-container alignment">
       <section className="contribute-section">
         <Heading1>CONTRIBUTING</Heading1>
         <p>Zo kun je deelnemen aan Demodam.</p>
@@ -14,7 +15,10 @@ const ContributionContent = () => {
       <section className="contribute-section">
         <Heading2>Word lid van ons Slack-kanaal</Heading2>
         <p>
-          <Link href="https://join.slack.com/t/samenorganiseren/shared_invite/zt-dex1d7sk-wy11sKYWCF0qQYjJHSMW5Q">
+          <Link
+            className="contribute-link"
+            href="https://join.slack.com/t/samenorganiseren/shared_invite/zt-dex1d7sk-wy11sKYWCF0qQYjJHSMW5Q"
+          >
             Word lid van de Common Ground Slack{" "}
           </Link>
           en sluit je aan bij de #Demodam-kanalen.
@@ -23,18 +27,25 @@ const ContributionContent = () => {
       <section className="contribute-section">
         <Heading2>Doe mee aan een hackathon</Heading2>
         <p>
-          Tijdens <Link href="#">hackathons</Link> bouwen we samen verder aan Demodam volgens onze doelstellingen.
+          Tijdens{" "}
+          <Link className="contribute-link" href="#">
+            hackathons
+          </Link>{" "}
+          bouwen we samen verder aan Demodam volgens onze doelstellingen.
         </p>
       </section>
       <section className="contribute-section">
         <Heading2>Wees actief op Github</Heading2>
         <p>
           Schiet een issue of pull request in, enz. Bekijk de{" "}
-          <Link href="https://github.com/demodam/demodam.org/issues?q=is%3Aissue+is%3Aopen+label%3A%22help+wanted%22">
+          <Link
+            className="contribute-link"
+            href="https://github.com/demodam/demodam.org/issues?q=is%3Aissue+is%3Aopen+label%3A%22help+wanted%22"
+          >
             issues met label good first issue
           </Link>{" "}
           en de{" "}
-          <Link href="https://github.com/demodam/demodam.org/labels/good%20first%20issue">
+          <Link className="contribute-link" href="https://github.com/demodam/demodam.org/labels/good%20first%20issue">
             issues met label good first issue
           </Link>{" "}
           om te beginnen.
@@ -48,7 +59,7 @@ const ContributionContent = () => {
         <Heading2>Publiceer Common Ground-componenten op Demodam</Heading2>
         <p>
           Demodam is een showcase-website voor open source-componenten. Volg deze{" "}
-          <Link href="https://github.com/demodam/demodam.org/blob/main/src/pages/join.md">
+          <Link className="contribute-link" href="https://github.com/demodam/demodam.org/blob/main/src/pages/join.md">
             stappen om componeneen op demodam te publiceren
           </Link>
           . Dat kan tijdens een hackathon, maar ook daarbuiten.
