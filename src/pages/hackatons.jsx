d74351f319935335b42d41b0c1282ea79a3b3951
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-22 15:19:14 +0100

    ✨ heckatons pagina added

---
+++ src/pages/hackatons.jsx
@@ -0,0 +1,35 @@
+import { SkipLink } from "@utrecht/component-library-react/dist/css-module";
+import * as React from "react";
+import Footer from "../components/Footer.js";
+import Content from "../components/content.jsx";
+import Header from "../components/header.jsx";
+import "./index.css";
+import "@utrecht/design-tokens/dist/index.css";
+import "../fonts/sourcesanspro.ttf";
+
+const IndexPage = () => {
+  return (
+    <>
+      <SkipLink href="#main">Naar de inhoud</SkipLink>
+      <div className="page-container">
+        <Header />
+        <div className="content-wrap">
+          <main id="main">
+            <Content />
+          </main>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => (
+  <>
+    <html lang="nl" dir="ltr" />
+    <meta charset="utf-8" />
+    <title>heckatons — Demodam</title>
+  </>
+);
