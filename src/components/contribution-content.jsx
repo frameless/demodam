22b1cf71ab0c4d2eac392eaf69932614a84130b7
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-22 16:38:00 +0100

    💄voor elke pagina die ik heb gemaakt dat het 1 css bestand nodig heeft voor de text bij de alignment

--- src/components/contribution-content.jsx
+++ src/components/contribution-content.jsx
@@ -2,12 +2,11 @@ import { Heading1 } from "@utrecht/component-library-react";
 import { Heading2 } from "@utrecht/component-library-react";
 import { Link } from "gatsby";
 import * as React from "react";
-import "/styles/contribution.css";
 import "/styles/alignment.css";
 
 const ContributionContent = () => {
   return (
-    <div className="contribute-container alignment">
+    <div className="alignment">
       <section className="contribute-section">
         <Heading1>CONTRIBUTING</Heading1>
         <p>Zo kun je deelnemen aan Demodam.</p>
@@ -16,7 +15,7 @@ const ContributionContent = () => {
         <Heading2>Word lid van ons Slack-kanaal</Heading2>
         <p>
           <Link
-            className="contribute-link"
+            className="alignment-link"
             href="https://join.slack.com/t/samenorganiseren/shared_invite/zt-dex1d7sk-wy11sKYWCF0qQYjJHSMW5Q"
           >
             Word lid van de Common Ground Slack{" "}
@@ -28,7 +27,7 @@ const ContributionContent = () => {
         <Heading2>Doe mee aan een hackathon</Heading2>
         <p>
           Tijdens{" "}
-          <Link className="contribute-link" href="#">
+          <Link className="alignment-link" href="/hackatons">
             hackathons
           </Link>{" "}
           bouwen we samen verder aan Demodam volgens onze doelstellingen.
@@ -39,13 +38,13 @@ const ContributionContent = () => {
         <p>
           Schiet een issue of pull request in, enz. Bekijk de{" "}
           <Link
-            className="contribute-link"
+            className="alignment-link"
             href="https://github.com/demodam/demodam.org/issues?q=is%3Aissue+is%3Aopen+label%3A%22help+wanted%22"
           >
             issues met label good first issue
           </Link>{" "}
           en de{" "}
-          <Link className="contribute-link" href="https://github.com/demodam/demodam.org/labels/good%20first%20issue">
+          <Link className="alignment-link" href="https://github.com/demodam/demodam.org/labels/good%20first%20issue">
             issues met label good first issue
           </Link>{" "}
           om te beginnen.
@@ -59,7 +58,7 @@ const ContributionContent = () => {
         <Heading2>Publiceer Common Ground-componenten op Demodam</Heading2>
         <p>
           Demodam is een showcase-website voor open source-componenten. Volg deze{" "}
-          <Link className="contribute-link" href="https://github.com/demodam/demodam.org/blob/main/src/pages/join.md">
+          <Link className="alignment-link" href="https://github.com/demodam/demodam.org/blob/main/src/pages/join.md">
             stappen om componeneen op demodam te publiceren
           </Link>
           . Dat kan tijdens een hackathon, maar ook daarbuiten.
--- src/components/hackatons-content.jsx
+++ src/components/hackatons-content.jsx
@@ -6,29 +6,35 @@ import "/styles/alignment.css";
 const ContributionContent = () => {
   return (
     <div className="alignment">
-      <section className="contribute-section">
+      <section className="hackatons-section">
         <Heading1>Demodam Hackathon III: 20-24 juni 2022</Heading1>
         <p>Demodam Hackathon III vindt plaats tijdens de Week van de (Digitale) Dienstverlening. Doe ook mee! </p>
         <p>
-          <Link href="https://weekvandedienstverlening.pleio.nl/demodam">
+          <Link className="alignment-link" href="https://weekvandedienstverlening.pleio.nl/demodam">
             Alle informatie over Demodam Hackathon III
           </Link>
           .
         </p>
       </section>
-      <section className="contribute-section">
+      <section className="hackatons-section">
         <Heading1>Vorige Demodam Hackathons</Heading1>
         <ul>
           <li>
             Demodam Hackathon I: 5-9 juli 2021{" "}
-            <Link href="https://docs.google.com/presentation/d/1ueLpnCIA06f05uuSTop8DU9olLb0M34iR_HrjsebRfI">
+            <Link
+              className="alignment-link"
+              href="https://docs.google.com/presentation/d/1ueLpnCIA06f05uuSTop8DU9olLb0M34iR_HrjsebRfI"
+            >
               Informatie, programma and resultaten
             </Link>
             .
           </li>
           <li>
             Demodam Hackathon II: 1-5 november 2021{" "}
-            <Link href="https://docs.google.com/presentation/d/1SJgPPEyf5I2tFjAzE0tRWqG64BC0LicFAnRUYPT4YQ4/edit#slide=id.p">
+            <Link
+              className="alignment-link"
+              href="https://docs.google.com/presentation/d/1SJgPPEyf5I2tFjAzE0tRWqG64BC0LicFAnRUYPT4YQ4/edit#slide=id.p"
+            >
               Informatie, programma and resultaten
             </Link>
             .
