29ae702789093768ba431b8b6992e11d724abc87
Rids <74712581+Ridsko@users.noreply.github.com>
2023-02-23 14:17:30 +0100

    ✨Governance page aangemaakt

---
+++ src/pages/governance.jsx
@@ -0,0 +1,34 @@
+import { SkipLink } from "@utrecht/component-library-react/dist/css-module";
+import * as React from "react";
+import Footer from "../components/footer.jsx";
+import Content from "../components/governance-content.jsx";
+import Header from "../components/header.jsx";
+import "./index.css";
+import "@utrecht/design-tokens/dist/index.css";
+
+const Governance = () => {
+  return (
+    <>
+      <SkipLink href="#main">Naar de inhoud</SkipLink>
+      <div className="page-container">
+        <Header />
+        <div className="content-wrap">
+          <main id="main">
+            <Content />
+          </main>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Governance;
+
+export const Head = () => (
+  <>
+    <html lang="nl" dir="ltr" />
+    <meta charset="utf-8" />
+    <title>Governance Page — Demodam</title>
+  </>
+);
