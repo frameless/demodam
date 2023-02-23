343358b82bc817de361884c7e56d50941170189e
Rids <74712581+Ridsko@users.noreply.github.com>
2023-02-23 12:04:00 +0100

    ✨ Link header naar contact

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
             </Link>
           </li>
           <li class="nav-item">
-            <Link href="#" class="nav-link">
+            <Link href="/contact" class="nav-link">
               Contact
             </Link>
           </li>
