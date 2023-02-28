34dda873fccdcba336aaaccc5adc161a29b4cc3a
scar055 <48519289+scar055@users.noreply.github.com>
2023-02-28 11:46:20 +0100

    🎨 image naar gatsby image verandert

--- src/components/blackbar.jsx
+++ src/components/blackbar.jsx
@@ -1,33 +1,65 @@
-import { Image } from "@utrecht/component-library-react";
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
-import conduction from "../images/conduction.png";
-import dimpact from "../images/dimpact.png";
-import foundation from "../images/foundation-for-public-code.png";
-import kadaster from "../images/kadaster.png";
-import maykinmedia from "../images/maykinmedia.png";
 import "/styles/blackbar.css";
 
-const Blackbar = () => (
-  <section className="blackbar-section">
-    <div className="blackbar-container">
-      <div className="blackbar-image-block">
-        <a href="#">
-          <Image className="blackbar-image" alt="bedrijfs logo van conduction." src={conduction} />
-        </a>
-        <a href="#">
-          <Image className="blackbar-image" alt="bedrijfs logo van maykin media." src={maykinmedia} />
-        </a>
-        <a href="#">
-          <Image className="blackbar-image" alt="bedrijf logo dimpact." src={dimpact} />
-        </a>
-        <a href="#">
-          <Image className="blackbar-image" alt="bedrijfs logo kadaster" src={kadaster} />
-        </a>
-        <a href="#">
-          <Image className="blackbar-image" alt="bedrijfs logo van foundation for public code" src={foundation} />
-        </a>
+const Blackbar = () => {
+  return (
+    <section className="blackbar-section">
+      <div className="blackbar-container">
+        <div className="blackbar-image-block">
+          <a href="#">
+            <StaticImage
+              height={"100%"}
+              width={"100%"}
+              className="blackbar-image"
+              alt="bedrijfs logo van conduction."
+              src="../images/conduction.png"
+              imgStyle={{ objectFit: "contain" }}
+            />
+          </a>
+          <a href="#">
+            <StaticImage
+              height={"100%"}
+              width={"100%"}
+              className="blackbar-image"
+              alt="bedrijfs logo van maykin media."
+              src="../images/maykinmedia.png"
+              imgStyle={{ objectFit: "contain" }}
+            />
+          </a>
+          <a href="#">
+            <StaticImage
+              height={"100%"}
+              width={"100%"}
+              className="blackbar-image"
+              alt="bedrijf logo dimpact."
+              src="../images/dimpact.png"
+              imgStyle={{ objectFit: "contain" }}
+            />
+          </a>
+          <a href="#">
+            <StaticImage
+              height={"100%"}
+              width={"100%"}
+              className="blackbar-image"
+              alt="bedrijfs logo kadaster"
+              src="../images/kadaster.png"
+              imgStyle={{ objectFit: "contain" }}
+            />
+          </a>
+          <a href="#">
+            <StaticImage
+              height={"100%"}
+              width={"100%"}
+              className="blackbar-image"
+              alt="bedrijfs logo van foundation for public code"
+              src="../images/foundation-for-public-code.png"
+              imgStyle={{ objectFit: "contain" }}
+            />
+          </a>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 export default Blackbar;
