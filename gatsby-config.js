623df1ada47ac44c57a87174732c73ae3ed51508
Robbert Broersma <git@robbertbroersma.nl>
2023-03-03 17:41:06 +0100

    build: use BASE_URL for github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,6 @@ module.exports = {
     title: `demodam2`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  pathPrefix: process.env.BASE_URL ?? "",
   plugins: [`gatsby-plugin-image`, `gatsby-plugin-sharp`, `gatsby-transformer-sharp`],
 };
