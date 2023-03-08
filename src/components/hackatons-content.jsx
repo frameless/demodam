d6cb2b71c4af5c0a47c8d42781971aebc4b2448e
scar055 <48519289+scar055@users.noreply.github.com>
2023-03-08 10:52:45 +0100

    🔥 removed classes die niet meer nodig zijn

--- src/components/hackatons-content.jsx
+++ src/components/hackatons-content.jsx
@@ -12,7 +12,7 @@ const ContributionContent = () => {
           Demodam Hackathon III vindt plaats tijdens de Week van de (Digitale) Dienstverlening. Doe ook mee!{" "}
         </Paragraph>
         <Paragraph>
-          <Link className="alignment-link" href="https://weekvandedienstverlening.pleio.nl/demodam">
+          <Link href="https://weekvandedienstverlening.pleio.nl/demodam">
             Alle informatie over Demodam Hackathon III
           </Link>
           .
@@ -23,20 +23,14 @@ const ContributionContent = () => {
         <UnorderedList>
           <UnorderedListItem>
             Demodam Hackathon I: 5-9 juli 2021{" "}
-            <Link
-              className="alignment-link"
-              href="https://docs.google.com/presentation/d/1ueLpnCIA06f05uuSTop8DU9olLb0M34iR_HrjsebRfI"
-            >
+            <Link href="https://docs.google.com/presentation/d/1ueLpnCIA06f05uuSTop8DU9olLb0M34iR_HrjsebRfI">
               Informatie, programma and resultaten
             </Link>
             .
           </UnorderedListItem>
           <UnorderedListItem>
             Demodam Hackathon II: 1-5 november 2021{" "}
-            <Link
-              className="alignment-link"
-              href="https://docs.google.com/presentation/d/1SJgPPEyf5I2tFjAzE0tRWqG64BC0LicFAnRUYPT4YQ4/edit#slide=id.Paragraph"
-            >
+            <Link href="https://docs.google.com/presentation/d/1SJgPPEyf5I2tFjAzE0tRWqG64BC0LicFAnRUYPT4YQ4/edit#slide=id.Paragraph">
               Informatie, programma and resultaten
             </Link>
             .
