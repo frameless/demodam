5cbb893aff97f61d92ffa8ed1729978b090f5252
scar055 <48519289+scar055@users.noreply.github.com>
2023-03-10 15:47:44 +0100

    🎨 .jsx weggehaalt voor tsx bestanden

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import { Document, SkipLink } from "@utrecht/component-library-react/dist/css-module";
 import * as React from "react";
-import Blackbar from "../components/blackbar.jsx";
-import Content from "../components/content.jsx";
-import Footer from "../components/footer.jsx";
+import Blackbar from "../components/blackbar";
+import Content from "../components/content";
+import Footer from "../components/footer";
 import Header from "../components/header";
-import Hero from "../components/hero.jsx";
-import Timeline from "../components/timeline.jsx";
+import Hero from "../components/hero";
+import Timeline from "../components/timeline";
 import "/styles/index.css";
 import "@fontsource/source-sans-pro/400.css";
 import "@fontsource/source-sans-pro/400-italic.css";
